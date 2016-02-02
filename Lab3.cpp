7ee5aac82f5fd27d39641a93dae2552c27ab1766
Tony <rednik111@gmail.com>
2016-02-02 11:46:55 -0800

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,2 +1,21 @@
-const int MAX=10;
-char pwd[MAX];
+/************************************************************
+ CIS 22B - section 04Y
+ Lab2:
+ Author: HUA XIA
+ Date: Jan 26th 2016
+ *************************************************************/
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cctype>
+using namespace std;
+int main()
+{
+  const int MAX=10;
+  char pwd[MAX];
+
+  system("pause");
+  return 0;
+  
+}
