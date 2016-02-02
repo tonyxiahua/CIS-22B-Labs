2587ff8d3e5c615ed1cd1982b69190761b68fd30
HUA XIA <rednik111@gmail.com>
2016-02-02 15:19:21 -0800

    Update Lab3.cpp
    Fixed the Bool problem of the do while loop

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -1,42 +1,40 @@
 /************************************************************
- CIS 22B - section 04Y
- Lab3:
- Author: HUA XIA
- Date: Feb 2nd 2016
- *************************************************************/
+CIS 22B - section 04Y
+Lab3:
+Author: HUA XIA
+Date: Feb 2nd 2016
+*************************************************************/
 
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <cctype>
-#include <>
+//#include <>
 using namespace std;
 bool test(char *);
 
 int main()
 {
-  const int MAX=10;
-  char pwd[MAX]={'\0'};
-
-  do
-  {
-   cout << "Enter password: " <<endl;
-   cin.getline(pwd, MAX);
-   cout << "You entered " << pwd <<endl;
-  }
-
-  while(!test(pwd));
-
-  system("pause");
-  return 0;
+	const int MAX = 20;
+	char pwd[MAX]; //= { '\0' };
+	do
+	{
+		cout << "Enter password: " << endl;
+		cin.getline(pwd, MAX);
+		cout << "You entered " << pwd << endl;
+	}
+	while (!test(pwd) && strlen(pwd) > 20);
+	cout << "You don't have enough numebr of digits or characters" << endl;
+	system("pause");
+	return 0;
 }
 
 bool test(char *p)
 {
- if (strlen(p) < 10)
- {
-  return false;
- }
- else
-  return ture;
+	if (strlen(p) < 20)
+	{
+		return false;
+	}
+	else
+		return true;
 }
